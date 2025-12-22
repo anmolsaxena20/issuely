4c0da46c916d17629b6d4e0893700a21016e9357
Anarok404 <anmolsxn2005@gmail.com>
2025-12-22 11:15:16 +0530

    Minor Changes

--- backend/src/middlewares/role.middleware.js
+++ backend/src/middlewares/role.middleware.js
@@ -14,3 +14,17 @@ export const requireLead = (req, res, next) => {
 
   next();
 };
+export const requireStaff = (req, res, next) => {
+  if (req.user.role !== "staff") {
+    return res.status(403).json({ message: "Staff access required" });
+  }
+
+  next();
+};
+export const requireStudent = (req, res, next) => {
+  if (req.user.role !== "student") {
+    return res.status(403).json({ message: "Lead access required" });
+  }
+
+  next();
+};
